1c1e8ff0066a2f543d0759b48cf2b3358363f241
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-18 00:20:12 +0400

    [feature] - load plugins by config object analysis
    https://github.com/mywebclass/mwc_meta/issues/7

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ var mwcKernel = require('mwc_kernel');
 
 module.exports = exports = function (config) {
   var MWC = mwcKernel(config);
-  MWC.extendApp(function(core){
+
+//basic frontend
+  MWC.extendApp(function (core) {
     core.app.locals.delimiters = '[[ ]]';
   });
   MWC.usePlugin(require('mwc_plugin_hogan_express'));
+//end of basic frontend
 
+//static html auth/register and edit my profile plugins
   MWC.usePlugin(require('mwc_plugin_welcome'));
   MWC.usePlugin(require('mwc_plugin_my_profile'));
-  MWC.usePlugin(require('mwc_plugin_notify_by_email'));
 
+  //enable plugin to send emails
+  if (config.emailConfig) {
+    MWC.usePlugin(require('mwc_plugin_notify_by_email'));
+  }
+
+  //rest api for mongoose models
   MWC.usePlugin(require('mwc_plugin_rest'));
-  MWC.usePlugin(require('mwc_plugin_spine'));
+
+
+  //task queue
+  if (config.spine) {
+    MWC.usePlugin(require('mwc_plugin_spine'));
+  }
 
   return MWC;
 };
