5ddca95356d90b84a88becc4bfbd9148641555d7
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-20 02:12:51 +0400

    [feature] publish assets from 3rd party modules [feature] publish assets from kabam modules placeholder

--- bin/kabam.js
+++ bin/kabam.js
@@ -31,7 +31,7 @@ program
           });
         },
         'hostUrl': function (cb) {
-          program.prompt('What is the hostname of your server? Default is "' +(''+ os.hostname()+'').green+ '": ', function (name) {
+          program.prompt('What is the hostname of your server? Default is "' + ('' + os.hostname() + '').green + '": ', function (name) {
             cb(null, name);
           });
         },
@@ -51,98 +51,100 @@ program
           });
         }
       }, function (err, input) {
-                async.parallel({
-          'createConfigJson':function(cb){
+        async.parallel({
+          'createConfigJson': function (cb) {
             var config = {
               'emailConfig': 'myemail@gmail.com:myLoooongPassword',
               "passport": {
-              "GITHUB_CLIENT_ID": "--insert-github-client-id-here--",
+                "GITHUB_CLIENT_ID": "--insert-github-client-id-here--",
                 "GITHUB_CLIENT_SECRET": "--insert-github-client-secret-here--",
                 "TWITTER_CONSUMER_KEY": "--insert-twitter-consumer-key-here--",
                 "TWITTER_CONSUMER_SECRET": "--insert-twitter-consumer-secret-here--",
                 "FACEBOOK_APP_ID": "--insert-facebook-app-id-here--",
                 "FACEBOOK_APP_SECRET": "--insert-facebook-app-secret-here--"
-            },
+              },
               "spine": {
-              'domains': ['urgentTasks']
-            }};
+                'domains': ['urgentTasks']
+              }};
 
-            if(input.hostUrl){
+            if (input.hostUrl) {
               config.hostUrl = input.hostUrl;
             } else {
               config.hostUrl = os.hostname();
             }
 
-            if(input.mongoUrl){
+            if (input.mongoUrl) {
               config.mongoUrl = input.mongoUrl;
             } else {
               config.mongoUrl = 'mongo://localhost/kabam_dev';
             }
 
-            if(input.redisUrl){
+            if (input.redisUrl) {
               config.redisUrl = input.redisUrl;
             } else {
               config.redisUrl = 'redis://localhost:6379';
             }
 
-            fs.writeFile(process.cwd()+'/config.json',JSON.stringify(config, null, 2),cb);
+            fs.writeFile(process.cwd() + '/config.json', JSON.stringify(config, null, 2), cb);
           },
-          'createPackagejson':function(cb){
+          'createPackagejson': function (cb) {
             var
-              projectName = (input.projectName)?(input.projectName):'my-kabam-project',
-              gitHubName = (input.gitHubName)?(input.gitHubName):'johgdoe',
+              projectName = (input.projectName) ? (input.projectName) : 'my-kabam-project',
+              gitHubName = (input.gitHubName) ? (input.gitHubName) : 'johgdoe',
               packageJson = {
-              "name": projectName,
-              "version": "0.0.1",
-              "description": "My Kabam project",
-              "main": "index.js",
-              "engines": {
-                "node": "0.10.x",
-                "npm": "1.3.x"
-              },
-              "scripts": {
-                "start": "node index.js"
-              },
-              "bin":{
-                "kabam":"bin/kabam.js"
-              },
-              "repository": {
-                "type": "git",
-                "url": "git://github.com/"+gitHubName+"/"+projectName+".git"
-              },
-              "keywords": [],
-              "author": gitHubName,
-              "license": "MIT",
-              "contributors": [
-                {
-                  "name": gitHubName,
-                  "url": "https://github.com/"+gitHubName
+                "name": projectName,
+                "version": "0.0.1",
+                "description": "My Kabam project",
+                "main": "index.js",
+                "engines": {
+                  "node": "0.10.x",
+                  "npm": "1.3.x"
+                },
+                "scripts": {
+                  "start": "node index.js"
+                },
+                "bin": {
+                  "kabam": "bin/kabam.js"
+                },
+                "repository": {
+                  "type": "git",
+                  "url": "git://github.com/" + gitHubName + "/" + projectName + ".git"
+                },
+                "keywords": [],
+                "author": gitHubName,
+                "license": "MIT",
+                "contributors": [
+                  {
+                    "name": gitHubName,
+                    "url": "https://github.com/" + gitHubName
+                  }
+                ],
+                "dependencies": {
+                  "async": "*",
+                  "commander": "1.3.2",
+                  "ncp": "*",
+                  "colors": "*",
+                  "kabam": "git+https://github.com/mykabam/kabam.git"
+                },
+                "devDependencies": {},
+                "bugs": {
+                  "url": "https://github.com/" + gitHubName + "/" + projectName + "/issues"
                 }
-              ],
-              "dependencies": {
-                "async": "*",
-                "commander":"1.3.2",
-                "ncp":"*",
-                "colors":"*",
-                "kabam": "git+https://github.com/mykabam/kabam.git"
-              },
-              "devDependencies": {},
-              "bugs": {
-                "url": "https://github.com/"+gitHubName+"/"+projectName+"/issues"
-              }
-            };
-            fs.writeFile(process.cwd()+'/package.json',JSON.stringify(packageJson, null, 2),cb);
+              };
+            fs.writeFile(process.cwd() + '/package.json', JSON.stringify(packageJson, null, 2), cb);
           },
-          'moveFiles':function(cb){
-            ncp(__dirname+'/../skel', process.cwd(), {'clobber':true}, cb);
+          'moveFiles': function (cb) {
+            ncp(__dirname + '/../skel', process.cwd(), cb);
           }
-        },function(err,actionsDone){
-          if(err) throw err;
+        }, function (err, actionsDone) {
+          if (err) {
+            throw err;
+          }
           console.log('Project created!'.green);
+          console.log('We need to install all needed modules by running ' + '$ npm install'.bold  + ' in the folder on newly created project!');
+          console.log('Than we need to run ' + '$ kabam.js publishAssets'.bold + ' to install views, public files and other things from plugins');
           process.exit(0);
         });
-
-
       });
     } else {
       console.error('This directory is not empty! Unable to process'.red);
@@ -150,53 +152,95 @@ program
     }
   });
 
-program
-  .command('publishAssets')
-  .description('Publish modules templates, css, javascripts and images')
-  .action(function () {
-    console.log('Publishing...');
+function publishAssets(pathToPackageJson){
+  var packageJson= require(pathToPackageJson),
+   packages = [];
+  for (var key in packageJson.dependencies) {
+    packages.push(key);
+  }
+
+  async.each(packages, function (packageName, cb) {
+    if (/^kabam\-plugin\-[0-9a-z\-]+$/.test(packageName)) {
+      console.log('We found ' + ('' + packageName).green + ' that looks like kabam plugin!');
+      var assetsPath = '' + process.cwd() + '/node_modules/' + packageName + '/assets';
+
+      if(fs.existsSync(assetsPath)){
+        var assets = fs.readdirSync('' + process.cwd() + '/node_modules/' + packageName + '/assets');
+        if (assets.indexOf('views') !== -2) {
+          console.log('We found views for ' + ('' + packageName).green + '!');
+        }
+        if (assets.indexOf('public') !== -2) {
+          console.log('We found public for ' + ('' + packageName).green + '!');
+        }
+        if (assets && assets.length > 0) {
+          console.log('Installing assets for plugin ' + ('' + packageName).green + '!');
+          ncp(assetsPath, process.cwd(), cb);
+        } else {
+          console.log('There is no assets for plugin ' + ('' + packageName).green + '!');
+          cb(null);
+        }
+      } else {
+        console.log('There is no assets for plugin ' + ('' + packageName).green + '!');
+        cb(null);
+      }
+    } else {
+      cb(null);
+    }
+  }, function (err) {
+    if (err) {
+      throw err;
+    }
     process.exit(0);
   });
 
 
+}
+
+
 program
-  .command('procfile')
-  .description('create Procfile for Foreman or nodeForeman')
+  .command('publishAssets')
+  .description('Publish templates(views), css, javascripts and images from kabam plugins ')
   .action(function () {
-    if (packageJson.scripts && packageJson.scripts.start) {
-      var startCommand = packageJson.scripts.start;
-//      console.log('We found command to start this application  - '+startCommand);
 
-      var procFileText = 'web: ' + startCommand + '\n';
-      fs.exists(cwd + '/Procfile', function (isExists) {
-        if (isExists) {
-          console.error('We already have Procfile created!'.red);
-          process.exit(1);
-        } else {
-//          console.log('Trying to write');
-//          console.log(procFileText);
-//          console.log(cwd+'/Procfile');
-
-          fs.writeFile(cwd + '/Procfile', procFileText, function (err) {
-            if (err) {
-              throw err;
-            }
-            console.log('Procfile created successfully! You can edit it if you want.'.green);
-            process.exit(0);
-          });
-        }
-      });
-
+    if (fs.existsSync(process.cwd() + '/package.json')) {
+      console.log('Publishing assets from 3rdParty modules');
+      publishAssets(process.cwd() + '/package.json');
     } else {
-      console.error('We need this project to be run by `npm start`'.red);
+      console.error('Error!'.red);
+      console.log('This directory do not have nodejs project in it...');
       process.exit(1);
     }
+
+    console.log('Publishing assets from kabam modules');
+
+//    if (fs.existsSync(process.cwd() + '/package.json')) {
+//      publishAssets(process.cwd() + '/package.json');
+//    } else {
+//      console.error('Error!'.red);
+//      console.log('This directory do not have nodejs project in it...');
+//      process.exit(1);
+//    }
+
+
+  });
+
+program
+  .command('backupMongo <filename>')
+  .description('try to backup mongo database into file')
+  .action(function () {
+    console.log('Will create it soon...');
+    process.exit(0);
+  });
+
+program
+  .command('restoreMongo <filename>')
+  .description('try to restore mongo database from file')
+  .action(function () {
+    console.log('Will create it soon...');
+    process.exit(0);
   });
 
 program.on('--help', function () {
-  console.log(' Examples');
-  console.log((' $ kabam create').bold);
-  console.log((' $ kabam procfile').bold);
 });
 
 console.log(('Kabam-Cli version ' + pkg.version).bold.yellow.redBG);
