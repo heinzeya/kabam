af17cf7aa13903443afad5401108e26a265f0d5d
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-08-23 02:07:42 +0400

    [feature] - automatic documentation generation... probably it works ok

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,41 @@ module.exports = function (grunt) {
         src: '<%= simplemocha.all.src %>'
       }
     },
+    clean: {
+      docs: [ 'results/docs' ]
+    },
+    copy: {
+      readme: {
+        src: 'README.md',
+        dest: 'results/index.ngdoc'
+      }
+    },
+    ngdocs: {
+      options: {
+        dest: 'results/docs',
+        title: 'KabamKernel',
+        startPage: '/api'
+      },
+      api: {
+        src: [
+          'results/index.ngdoc',
+          'node_modules/kabam-kernel/bins/*.js',
+          'node_modules/kabam-kernel/example/*.js',
+          'node_modules/kabam-kernel/lib/*.js',
+          'node_modules/kabam-kernel/models/userModel.js',
+          'node_modules/kabam-kernel/index.js',
+          'node_modules/kabam-kernel/example/plugin.example.js',
+          'node_modules/kabam-plugin-hogan/index.js',
+          'node_modules/kabam-plugin-my-profile/index.js',
+          'node_modules/kabam-plugin-notify-email/index.js',
+          'node_modules/kabam-plugin-private-message/index.js',
+          'node_modules/kabam-plugin-rest/index.js',
+          'node_modules/kabam-plugin-spine/index.js',
+          'node_modules/kabam-plugin-spine/welcome.js',
+        ],
+        title: 'MWC Kernel API'
+      }
+    },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
@@ -58,11 +93,15 @@ module.exports = function (grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-ngdocs');
 
   // Tasks
   grunt.registerTask('test', ['simplemocha']);
   grunt.registerTask('testci', ['simplemocha:ci']);
+  grunt.registerTask('docs', ['clean:docs', 'copy:readme', 'ngdocs']);
   // Default task.
   grunt.registerTask('default', ['jshint', 'test']);
 
