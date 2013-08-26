dbc7b2b70ec6752cee37a529a11fee806a0b9564
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-26 17:50:26 +0400

    [feature] - load oath parameters from enviroment

--- example/example.js
+++ example/example.js
@@ -8,12 +8,12 @@ var kabam = Kabam({
 //  'redis': 'redis://mwcKernel:@localhost:6379', - we use it from enviroment
   'emailConfig': ((process.env.emailConfig) ? (process.env.emailConfig) : 'myemail@gmail.com:1234567'),
   "passport": {
-    "GITHUB_CLIENT_ID": "--insert-github-client-id-here--",
-    "GITHUB_CLIENT_SECRET": "--insert-github-client-secret-here--",
-    "TWITTER_CONSUMER_KEY": "--insert-twitter-consumer-key-here--",
-    "TWITTER_CONSUMER_SECRET": "--insert-twitter-consumer-secret-here--",
-    "FACEBOOK_APP_ID": "--insert-facebook-app-id-here--",
-    "FACEBOOK_APP_SECRET": "--insert-facebook-app-secret-here--"
+    "GITHUB_CLIENT_ID": ((process.env.GITHUB_CLIENT_ID) ? (process.env.GITHUB_CLIENT_ID) : '--insert-github-client-id-here--'),
+    "GITHUB_CLIENT_SECRET": ((process.env.GITHUB_CLIENT_SECRET) ? (process.env.GITHUB_CLIENT_SECRET) : '--insert-github-client-secret-here--'),
+    "TWITTER_CONSUMER_KEY": ((process.env.TWITTER_CONSUMER_KEY) ? (process.env.TWITTER_CONSUMER_KEY) : '--insert-twitter-consumer-key-here--'),
+    "TWITTER_CONSUMER_SECRET": ((process.env.TWITTER_CONSUMER_SECRET) ? (process.env.TWITTER_CONSUMER_SECRET) : '--insert-twitter-consumer-secret-here--'),
+    "FACEBOOK_APP_ID": ((process.env.FACEBOOK_APP_ID) ? (process.env.FACEBOOK_APP_ID) : '--insert-facebook-app-id-here--'),
+    "FACEBOOK_APP_SECRET": ((process.env.FACEBOOK_APP_SECRET) ? (process.env.FACEBOOK_APP_SECRET) : '--insert-facebook-app-secret-here--')
   },
   "spine": {
     'domains': ['urgentTasks']
